c26ec6468c085f2cfcfc7c8da03c55d431789e20
kazuto28 <konndoukazuto45@gmail.com>
2021-06-16 18:38:50 +0900

    combine patches

--- script/install_youtube-dlg.sh
+++ script/install_youtube-dlg.sh
@@ -17,17 +17,14 @@ info "ファイルをダウンロードしています..."
 curl -# http://ppa.launchpad.net/nilarimogard/webupd8/ubuntu/pool/main/y/youtube-dlg/youtube-dlg_0.4-1~webupd8~bionic9_all.deb -o /tmp/youtube-dlg.deb || abort "ダウンロードが中断されました"
 curl -L# "https://raw.githubusercontent.com/kazuto28/crostini_easy_setup/master/data/youtube-dlg.patch" -o /tmp/youtube-dlg.patch || abort "ダウンロードが中断されました"
 curl -L# "https://raw.githubusercontent.com/kazuto28/crostini_easy_setup/master/data/youtube_dl_gui.mo" -o /tmp/youtube_dl_gui.mo || abort "ダウンロードが中断されました"
-curl -L# "https://raw.githubusercontent.com/kazuto28/crostini_easy_setup/master/data/youtube-dlg_jp.patch" -o /tmp/youtube-dlg_jp.patch || abort "ダウンロードが中断されました"
 
 info "インストールしています..."
 sudo apt install -y /tmp/youtube-dlg.deb
-
-info "Patchを適用しています..."
-sudo patch $YDLG_DIR/mainframe.py < /tmp/youtube-dlg.patch
-
-info "言語に日本語を追加しています..."
 sudo mkdir -p $YDLG_DIR/locale/ja_JP/LC_MESSAGES
 sudo cp /tmp/youtube_dl_gui.mo $YDLG_DIR/locale/ja_JP/LC_MESSAGES/
-sudo patch $YDLG_DIR/optionsframe.py < /tmp/youtube-dlg_jp.patch
+
+info "Patchを適用しています..."
+sudo apt install patch
+sudo patch -d $YDLG_DIR -p1 < /tmp/youtube-dlg.patch
 
 info "インストールが完了しました"
