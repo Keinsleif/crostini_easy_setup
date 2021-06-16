6ff4f79a869c306f3ed58da6e887ad97090163a5
kazuto28 <36069591+kazuto28@users.noreply.github.com>
2021-06-16 12:28:18 +0900

    Fix youtube-dlg installer

--- script/install_youtube-dlg.sh
+++ script/install_youtube-dlg.sh
@@ -15,9 +15,9 @@ YDLG_DIR=/usr/lib/python2.7/dist-packages/youtube_dl_gui
 
 info "ファイルをダウンロードしています..."
 curl -# http://ppa.launchpad.net/nilarimogard/webupd8/ubuntu/pool/main/y/youtube-dlg/youtube-dlg_0.4-1~webupd8~bionic9_all.deb -o /tmp/youtube-dlg.deb || abort "ダウンロードが中断されました"
-curl -L# "https://www.dropbox.com/s/bbs3yuyjp9m6z4l/youtube-dlg.patch?dl=0" -o /tmp/youtube-dlg.patch || abort "ダウンロードが中断されました"
-curl -L# "https://www.dropbox.com/s/ags0h52mhm2vdf7/youtube_dl_gui.mo?dl=0" -o /tmp/youtube_dl_gui.mo || abort "ダウンロードが中断されました"
-curl -L# "https://www.dropbox.com/s/sv1t529x3s6icey/youtube-dlg_jp.patch?dl=0" -o /tmp/youtube-dlg_jp.patch || abort "ダウンロードが中断されました"
+curl -L# "https://github.com/kazuto28/crostini_easy_setup/raw/master/data/youtube-dlg.patch" -o /tmp/youtube-dlg.patch || abort "ダウンロードが中断されました"
+curl -L# "https://github.com/kazuto28/crostini_easy_setup/raw/master/data/youtube_dl_gui.mo" -o /tmp/youtube_dl_gui.mo || abort "ダウンロードが中断されました"
+curl -L# "https://github.com/kazuto28/crostini_easy_setup/raw/master/data/youtube-dlg_jp.patch" -o /tmp/youtube-dlg_jp.patch || abort "ダウンロードが中断されました"
 
 info "インストールしています..."
 sudo apt install -y /tmp/youtube-dlg.deb
