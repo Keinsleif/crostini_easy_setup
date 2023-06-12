c47e58608eef6ff1d1c7c68fb7e51151d1f4c19c
Keinsleif <mountaindull@gmail.com>
2023-06-12 09:14:22 +0900

    Update install_youtube-dlg.sh
    rename repo username

--- script/install_youtube-dlg.sh
+++ script/install_youtube-dlg.sh
@@ -27,8 +27,8 @@ function usage {
 function install_ydlg {
 	info "ファイルをダウンロードしています..."
 	curl -# "http://ppa.launchpad.net/nilarimogard/webupd8/ubuntu/pool/main/y/youtube-dlg/youtube-dlg_0.4-1~webupd8~bionic9_all.deb" -o /tmp/youtube-dlg.deb || abort "ダウンロードが中断されました"
-	curl -L# "https://raw.githubusercontent.com/kazuto28/crostini_easy_setup/master/data/youtube-dlg.patch" -o /tmp/youtube-dlg.patch || abort "ダウンロードが中断されました"
-	curl -L# "https://raw.githubusercontent.com/kazuto28/crostini_easy_setup/master/data/youtube_dl_gui.mo" -o /tmp/youtube_dl_gui.mo || abort "ダウンロードが中断されました"
+	curl -L# "https://raw.githubusercontent.com/Keinsleif/crostini_easy_setup/master/data/youtube-dlg.patch" -o /tmp/youtube-dlg.patch || abort "ダウンロードが中断されました"
+	curl -L# "https://raw.githubusercontent.com/Keinsleif/crostini_easy_setup/master/data/youtube_dl_gui.mo" -o /tmp/youtube_dl_gui.mo || abort "ダウンロードが中断されました"
 
 	info "インストールしています..."
 	sudo apt install -y /tmp/youtube-dlg.deb
